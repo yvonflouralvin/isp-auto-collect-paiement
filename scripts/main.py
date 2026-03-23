c2e24e49caf50edd11601027e3bbf78f38d911d3
Yvon Flour Alvin <me@yvonflour.com>
2026-03-23 17:31:31 +0000

    dev

--- scripts/main.py
+++ scripts/main.py
@@ -9,15 +9,15 @@ import os
 # Config depuis variables d'environnement
 # -----------------------------
 
-LOGIN_URL = os.environ.get("LOGIN_URL", "https://apiisp.saas.cd/api/apps/auth/login/")
-SYNC_URL = os.environ.get("SYNC_URL", "https://apiisp.saas.cd/api/apps/isp_stage/sync-isp-paiements/")
-USERNAME = os.environ.get("USERNAME", "admin")
-PASSWORD = os.environ.get("PASSWORD", "Admin1")
+LOGIN_URL = os.environ.get("LOGIN_URL", "https://apiispdev.saas.cd/api/apps/auth/login/")
+SYNC_URL = os.environ.get("SYNC_URL", "https://apiispdev.saas.cd/api/apps/isp_stage/sync-isp-paiements/")
+USERNAME = os.environ.get("USERNAME", "ispdev_saas_cd")
+PASSWORD = os.environ.get("PASSWORD", "1234")
 INTERVAL_SECONDS = int(os.environ.get("INTERVAL_SECONDS", 5 * 60))  # 5 minutes par défaut
 
 # Dates de début et fin pour le script historique
 START_DATE_STR = os.environ.get("START_DATE", "2026-01-01")
-END_DATE_STR = os.environ.get("END_DATE", "2026-03-30")
+END_DATE_STR = os.environ.get("END_DATE", "2026-12-30")
 
 # Convertir les strings en objets date
 START_DATE = date.fromisoformat(START_DATE_STR)
